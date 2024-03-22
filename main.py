7bdc8381326b7113a2aea84bb53f051bdd59294b
Homeskills <137374823+Homeskills@users.noreply.github.com>
2024-03-22 18:54:37 -0400

    added yelp_scraper function to main.py and updated consolidate_reviews.ipynb

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
 import re
-
+import requests
+from bs4 import BeautifulSoup
 
 def convert_outscraper_file(csv_filepath):
     """
@@ -14,4 +15,20 @@ def convert_outscraper_file(csv_filepath):
     # Output new csv
     output_path = os.environ.get("output_path")
     df_selected.to_csv(output_path, index=False)
+
+
+def scrape_yelp(url):
+    """
+    Scrapes customer reviews (doesn't include business reply reviews) from a given yelp page. Effective as of March '24.
+    :param url: URL of yelp page to scrape reviews from
+    :return: list of reviews
+    """
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    regex = re.compile('.*comment__09f24__D0cxf.*') # If you want all reviews including business replies, replace 'comment__09f24__D0cxf' with just 'comment'. This value was found by inspecting review 'class'
+    results = soup.find_all('p', {'class': regex})
+    reviews = [result.text for result in results]
+    return reviews
+
+print(scrape_yelp('https://www.yelp.com/biz/madtree-brewing-cincinnati-3'))
 
