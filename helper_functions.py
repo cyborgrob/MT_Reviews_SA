3ee99dbb7bcd9b50b4a20466d89aa7f07cf0b12f
Homeskills <137374823+Homeskills@users.noreply.github.com>
2024-04-11 22:32:40 -0400

    update mt_train_test_split func

--- helper_functions.py
+++ helper_functions.py
@@ -13,20 +13,15 @@ def remove_blank_terms(file_path):
     filtered_df.to_csv('filtered_file.csv', index=False)
 
 
-def train_val_test_split(file_path, train=0.7, random_state=42):
+def mt_train_test_split(file_path, test_size=0.3, random_state=42):
     """Takes a file path of a complete dataset and creates shuffled train, validation, and test splits in a 70/15/15
     split by default."""
     # Load dataset
     data = pd.read_csv(file_path)
 
     # Split into training set and remaining data
-    train_data, remaining_data = train_test_split(data, test_size=(1 - train), random_state=random_state)
-
-    # Now split the remaining data into validation and test sets
-    validation_data, test_data = train_test_split(remaining_data, test_size=0.5, random_state=random_state)
+    train_data, test_data = train_test_split(data, test_size=test_size, random_state=random_state)
 
     # Save these to csv files
     train_data.to_csv(r'datasets\train_data.csv', index=False)
-    validation_data.to_csv(r'datasets\validation_data.csv', index=False)
     test_data.to_csv(r'datasets\test_data.csv', index=False)
-
